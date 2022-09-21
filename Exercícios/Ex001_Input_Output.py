bf1fbe1a1ac79dd62c1bc4f888a5c12506cff7a8
Perikson <darcperikson@msn.com>
2022-09-21 17:06:13 -0300

    Create Ex001_Input_Output.py

---
+++ Exercícios/Ex001_Input_Output.py
@@ -0,0 +1,14 @@
+""""""
+"""
+1 - Imprimir na tela a mensagem: 'Olá Mundo'.
+
+2 - Receber nome e idade do usuário e imprimi-los em um único print.
+
+# 1
+print('Olá Mundo')
+
+# 2
+nome = input('Digite seu nome: ') #Recebendo o nome do usuário
+idade = input('Digite sua idade: ') #Recebendo a idade do usuário
+print(f'Nome: {nome}. Idade: {idade}')
+"""
